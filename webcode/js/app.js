6c7a37e5f636548140a36cdba7e1faa9ebd63d2f
mxsryy <yomxsrynerd@gmail.com>
2025-10-28 10:21:47 +0700

    modal testing added, bugs fixed, repost adjustment

--- webcode/js/app.js
+++ webcode/js/app.js
@@ -1,1 +1,5 @@
-
+const modalElement = document.getElementById('myModal');
+const myModal = new bootstrap.Modal(modalElement);
+document.getElementById('openModalBtn').addEventListener('click', () => {
+    myModal.show();
+});
